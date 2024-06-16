a9fbbb942a9a80ebd746fffdad1a3852a86075fd
Yevheniy <yevheniykushnirenko@gmail.com>
2024-06-16 17:26:21 +0100

    created a map for my test game

---
+++ MyOwnPokemon/resources/colisiona_small.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="colisiona_small" tilewidth="4" tileheight="4" tilecount="9" columns="3">
+ <image source="Tiled/collision.png" width="12" height="12"/>
+</tileset>
