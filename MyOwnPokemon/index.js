1db09a6250f27eafa38b7fb4be8660fb30f7bb85
Yevheniy <yevheniykushnirenko@gmail.com>
2024-06-23 23:44:30 +0100

    implemented player movement and boundries/ colisions as well as foreground for player movement behind objects

--- MyOwnPokemon/index.js
+++ MyOwnPokemon/index.js
@@ -6,137 +6,14 @@ canva.height = 576;
 
 const c = canva.getContext('2d');
 
-class Resource {
-    constructor (src) {
-        
-        this.image = new Image()
-        this.image.src = src
-    }
-}
-
-class Map extends Resource {
-    constructor ({x, y, src}){
-        super(src)
-        this.x = x 
-        this.y = y
-
-        this.squareSize = 66
-        this.colisions = []
-        this.loadColisions()
-    }
-
-    loadColisions() {
-        var i = -1;
-        while((i = colisions.indexOf(1025, i+1)) != -1){
-
-            var boundry = new Boundry({
-                y: ((i/70)>>0)*this.squareSize + this.y,
-                x: (i%70)* this.squareSize + this.x,
-                side: this.squareSize
-            });
-            this.colisions.push(boundry);
-        }
-    }
-
-    draw() {
-        c.drawImage(this.image, this.x, this.y);
-        this.colisions.forEach((boundry) => boundry.draw());
-    }
-}
-
-class Player extends Resource {
-    constructor ({velocity, src}) {
-        super(src)
-        this.velocity = velocity;
-        this.x = (canva.width/2 - (this.image.width/4)/2)
-        this.y = (canva.height/2 - this.image.height/2)
-
-        this.boundryLeft = this.x
-        this.boundryRight = this.x + this.image.width/4
-        this.boundryTop = this.y - 2*this.image.height/3
-        this.boundryBottom = this.y - this.image.height
-    }
-
-    move(direction) {
-        //y + -> up ; y -  -> down; x + -> left  ; x -  -> rigth
-
-        const axis = direction === 'up' || direction === 'down' ? 'y' : 'x';
-        const sign = direction === 'up' || direction === 'left' ? 1 : -1;
-        
-        map.colisions.forEach((boundry) => {
-            if( this.boundryRight >= boundry.x &&
-                this.boundryLeft <= (boundry.x + boundry.side) &&
-                this.boundryTop >= (boundry.y - boundry.side) && 
-                this.boundryBottom <= boundry.y
-            ){
-                alert("boundry")
-            }
-     
-            boundry[axis] += this.velocity * sign
-        });
-        map[axis] += this.velocity * sign
- 
-        animate();
-    }
-
-    draw() {
-        c.drawImage(
-            this.image,
-            0,
-            0,
-            (this.image.width/4),
-            this.image.height, 
-            this.x, 
-            this.y,
-            (this.image.width/4),
-            this.image.height
-        );
-        player.boundryBox();
-    }
-
-    boundryBox() {
-        c.strokeRect(this.x, this.y+ 2*this.image.height/3, this.image.width/4, this.image.height/3);
-
-        c.fillStyle = "white"
-        c.fillRect(this.x, this.y+ 2*this.image.height/3, 5, 5);
-        c.fillStyle = "green"
-        c.fillRect(this.x, this.y+this.image.height, 5, -5);
-        c.fillStyle = "black"
-        c.fillRect(this.x + this.image.width/4, this.y+ 2*this.image.height/3, -5, 5);
-        c.fillStyle = "pink"
-        c.fillRect(this.x + this.image.width/4, this.y+this.image.height, -5, -5);
-    }
-}
-
-class Boundry {
-
-    constructor ({x, y, side}) {
-        this.x = x;
-        this.y = y;
-        this.side = side;
-
-    }
-
-    draw(){
-        c.fillStyle = 'red';
-
-        if(this.x < 0){
-            c.fillStyle = 'blue';
-            c.fillRect(this.x, this.y , 66, 66);
-        }
-       
-        c.fillRect(this.x, this.y , this.side, this.side)
-
-    }
-}
-
-
 
 const map = new Map({
-    x: -1865,
-    y: -680,
     src: './resources/Map.png'
 });
+
+const foreground = new Map({
+    src: './resources/Foreground.png'
+})
 
 const player = new Player({
     velocity: 4,
@@ -147,13 +24,29 @@ const player = new Player({
 function animate() {
         map.draw();
         player.draw();
+        foreground.draw()
 
+}
+
+var boundries = []
+
+function loadColisions() {
+    var i = -1;
+    while((i = colisions.indexOf(1025, i+1)) != -1){
+
+        var boundry = new Boundry({
+            y: ((i/70)>>0)*map.squareSize + map.y,
+            x: (i%70)* map.squareSize + map.x,
+            side: map.squareSize
+        });
+        boundries.push(boundry);
+    }
 }
 
 map.image.onload = () => {
     player.image.onload = () => {
         animate()
-        
+        loadColisions() 
     }
 }
 
